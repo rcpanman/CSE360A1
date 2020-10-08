b327bfb25cdb7ffd373e1f19d97ae48f21f2930d
panryan <panryan@localhost>
2020-10-07 18:06:58 -0700

    Initial Commit

---
+++ WHAT/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,58 @@
+package cse360assignment02;
+
+
+/**
+ * 
+ * @author panryan
+ * AddingMachine class has multiple methods intended to perform a task, majority are blank
+ *
+ */
+public class AddingMachine {
+	
+	/**
+	 * Integer to keep count of total
+	 */
+  private int total;
+  
+  /**
+   * Constructor; creates a new AddingMachine, assigns total a value of 0
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Getter that returns a value
+   * @return 0
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Performs some task, method is blank
+   * @param value Given integer value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Performs some task, method is blank
+   * @param value Given integer value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Method that returns a String object that represents value of the parameter 
+   */
+  public String toString () {
+    return "";
+  }
+  
+/**
+ * Method that performs some task, method is blank, does not return a value
+ */
+  public void clear() {
+  }
+}
