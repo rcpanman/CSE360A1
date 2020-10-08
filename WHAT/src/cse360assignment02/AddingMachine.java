634db17465815516da0bd89e66db21cd90b2b5af
panryan <panryan@localhost>
2020-10-07 19:58:01 -0700

    Final Commit

--- WHAT/src/cse360assignment02/AddingMachine.java
+++ WHAT/src/cse360assignment02/AddingMachine.java
@@ -7,52 +7,74 @@ package cse360assignment02;
  * AddingMachine class has multiple methods intended to perform a task, majority are blank
  *
  */
+
+
 public class AddingMachine {
 	
 	/**
 	 * Integer to keep count of total
 	 */
   private int total;
+  /**
+   * StringBuffer to store history
+   */
+  private StringBuffer history; 
   
   /**
-   * Constructor; creates a new AddingMachine, assigns total a value of 0
+   * Constructor; creates a new AddingMachine
    */
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+	  /**
+	   * Creates a new StringBuffer - history with 0 as initial string value
+	   */
+    history = new StringBuffer("0");
   }
   
   /**
-   * Getter that returns a value
-   * @return 0
+   * Getter that returns the current total value
+   * @return total
    */
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   /**
-   * Performs some task, method is blank
+   * Add method that adds parameter to the total variable
    * @param value Given integer value
    */
   public void add (int value) {
+	  total = value + total;
+	  /**
+	   * appends history, inserting '+' and adding the parameter value
+	   */
+	  history.append(" + "+value);
   }
 
   /**
-   * Performs some task, method is blank
+   * Subtract method that subtracts parameter from the total variable
    * @param value Given integer value
    */
   public void subtract (int value) {
+	  total = total - value;
+	  /**
+	   * appends history, inserting '-' and adding the parameter value 
+	   */
+	  history.append(" - "+value);
   }
 
   /**
-   * Method that returns a String object that represents value of the parameter 
+   * Method that returns history
+   * 
    */
   public String toString () {
-    return "";
+    return history.toString(); 
   }
   
 /**
- * Method that performs some task, method is blank, does not return a value
+ * Method deletes entire history 
  */
   public void clear() {
+	  history.delete(0, history.length());
+	  
   }
 }
